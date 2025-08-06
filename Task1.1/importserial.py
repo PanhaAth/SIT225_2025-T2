aae8213b7756ecd92856473997c46546fe621067
PanhaAth <167052149+PanhaAth@users.noreply.github.com>
2025-08-07 08:42:44 +1000

    Update importserial.py

--- Task1.1/importserial.py
+++ Task1.1/importserial.py
@@ -9,14 +9,14 @@ s = serial.Serial('COM5', baud_rate, timeout=5)
 
 while True: #infinite loop, keep running
 
-    #a random number between 9 and 50.
-    data_send = random.randint(5, 50)
+    #a random number between 1 and 10.
+    data_send = random.randint(1, 10)
     d = s.write((str(data_send) + '\r\n').encode('utf-8'))
     print(f"[{datetime.now().strftime('%H:%M:%S')}] SEND >>> {data_send} ({d} bytes)")
 
     received_data = s.readline().decode("utf-8").strip()
-    Sec = d/10 + 1
-    print(f"[{datetime.now().strftime('%H:%M:%S')}] RECEIVE <<< {d} sleep for {Sec}s")
-
+    if received_data.isdigit():
+        Sec = int(received_data)
+        print(f"[{datetime.now().strftime('%H:%M:%S')}] RECV <<< {Sec} sleep for {Sec}s")
     #Sleep for the calculated time
     time.sleep(Sec)
